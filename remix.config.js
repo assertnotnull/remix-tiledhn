28361b81f01044c700c07714b3fda6cdf5ec9b8a
Patrice Gauthier <patgauth@gmail.com>
2023-11-29 22:50:46 -0500

    fix remix config & add vercel dep

--- remix.config.js
+++ remix.config.js
@@ -1,15 +1,14 @@
 import { createRoutesFromFolders } from "@remix-run/v1-route-convention";
 
-/**
- * @type {import('@remix-run/dev').AppConfig}
- */
-export const cacheDirectory = "./node_modules/.cache/remix";
-export const ignoredRouteFiles = [
-  "**/.*",
-  "**/*.css",
-  "**/*.test.{js,jsx,ts,tsx}",
-];
-export function routes(defineRoutes) {
-  // uses the v1 convention, works in v1.15+ and v2
-  return createRoutesFromFolders(defineRoutes);
-}
+/** @type {import('@remix-run/dev').AppConfig} */
+export default {
+  appDirectory: "app",
+  assetsBuildDirectory: "public/build",
+  future: {},
+  ignoredRouteFiles: ["**/.*"],
+  publicPath: "/build/",
+  routes(defineRoutes) {
+    return createRoutesFromFolders(defineRoutes);
+  },
+  serverBuildPath: "build/index.js",
+};
