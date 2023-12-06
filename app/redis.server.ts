dd3fd1c83870cf37858d2d3b6e688ba14e56d5c5
Patrice Gauthier <patgauth@gmail.com>
2023-12-05 21:23:00 -0500

    remove redis env vars

--- app/redis.server.ts
+++ app/redis.server.ts
@@ -1,19 +1,6 @@
 import { createStorage } from "unstorage";
 import redisDriver from "unstorage/drivers/redis";
 import vercelKVDriver from "unstorage/drivers/vercel-kv";
-import { z } from "zod";
-
-const redisEnvSchema = z.object({
-  REDIS_HOST: z.string().min(1),
-  REDIS_PORT: z
-    .string()
-    .min(1)
-    .transform((v) => parseInt(v)),
-  REDIS_USERNAME: z.string().optional(),
-  REDIS_PASSWORD: z.string().optional(),
-});
-
-const config = redisEnvSchema.parse(process.env);
 
 export const cacheClient = createStorage({
   driver:
