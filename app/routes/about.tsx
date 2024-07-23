81ff531e2412c88fe50ef4f732876a15ad5683ac
Patrice Gauthier <patgauth@gmail.com>
2024-07-23 10:23:48 -0400

    lazy load image for faster render

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -2,6 +2,9 @@ import { useLoaderData } from "@remix-run/react";
 import { json } from "@remix-run/server-runtime";
 import { createHash } from "crypto";
 import { URL } from "url";
+import reactLazyLoadImage from "react-lazy-load-image-component";
+
+const LazyLoadImage = reactLazyLoadImage.LazyLoadImage;
 
 export function loader() {
   const emailHash = createHash("md5")
@@ -21,7 +24,10 @@ export default function About() {
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row">
-        <img src={data.imageUrl} className="max-w-sm rounded-full shadow-2xl" />
+        <LazyLoadImage
+          src={data.imageUrl}
+          className="max-w-sm rounded-full shadow-2xl"
+        />
         <div>
           <h1 className="text-5xl font-bold">Patrice Gauthier</h1>
           <p className="py-6">
