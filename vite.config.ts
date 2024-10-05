6bb28d3bf737103ac9556807fdb1e087ba893851
Patrice Gauthier <patgauth@gmail.com>
2024-10-04 22:50:33 -0400

    update based on a new project

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
+import path from "path";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 installGlobals();
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./app"),
+    },
+  },
   plugins: [
     remix({
       future: {
