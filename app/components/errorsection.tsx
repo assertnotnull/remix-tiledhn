20034b83ff52f178d2d1d957efe22ff3cea98af0
Patrice Gauthier <patgauth@gmail.com>
2024-10-27 22:24:38 -0400

    add better error section

---
+++ app/components/errorsection.tsx
@@ -0,0 +1,9 @@
+export default function ErrorDisplay() {
+  return (
+    <section className="px-6 pb-6 pt-20 mb-20 mx-auto">
+      <div className="flex justify-center align-bottom">
+        <div className="text-error">Failed to load</div>
+      </div>
+    </section>
+  );
+}
