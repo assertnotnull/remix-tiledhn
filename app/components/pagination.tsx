0912663f2dac546f8e4076cfd72bd6eec77f3aa5
Patrice Gauthier <patgauth@gmail.com>
2025-02-09 20:53:23 -0500

    make pagination to be a select on small screens

--- app/components/pagination.tsx
+++ app/components/pagination.tsx
@@ -5,26 +5,48 @@ const Paginate = ({ numberOfPages }: { numberOfPages: number }) => {
   const [searchParams, setSearchParams] = useSearchParams();
   const currentPage = searchParams.get("page") ?? "1";
 
+  const handlePageChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedPage = event.target.value;
+    const params = new URLSearchParams();
+    params.set("page", selectedPage);
+    setSearchParams(params);
+  };
+
   return (
     <div className="flex justify-center py-4 gap-1 fixed bottom-0 w-full bg-primary-content">
-      {new Array(numberOfPages).fill(0).map((_, i) => (
-        <button
-          key={i}
-          className={classNames("btn", {
-            "btn-active": i + 1 == +currentPage,
-          })}
-          onClick={() => {
-            const indexPlusOne = i + 1;
-            const params = new URLSearchParams();
-            if (+currentPage !== indexPlusOne) {
-              params.set("page", indexPlusOne.toString());
-              setSearchParams(params);
-            }
-          }}
+      <div className="hidden md:flex">
+        {new Array(numberOfPages).fill(0).map((_, i) => (
+          <button
+            key={i}
+            className={classNames("btn", {
+              "btn-active": i + 1 == +currentPage,
+            })}
+            onClick={() => {
+              const indexPlusOne = i + 1;
+              const params = new URLSearchParams();
+              if (+currentPage !== indexPlusOne) {
+                params.set("page", indexPlusOne.toString());
+                setSearchParams(params);
+              }
+            }}
+          >
+            {i + 1}
+          </button>
+        ))}
+      </div>
+      <div className="md:hidden">
+        <select
+          className="select select-bordered"
+          value={currentPage}
+          onChange={handlePageChange}
         >
-          {i + 1}
-        </button>
-      ))}
+          {new Array(numberOfPages).fill(0).map((_, i) => (
+            <option key={i} value={i + 1}>
+              {i + 1}
+            </option>
+          ))}
+        </select>
+      </div>
     </div>
   );
 };
